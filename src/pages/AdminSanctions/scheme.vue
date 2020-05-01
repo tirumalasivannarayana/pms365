ec86a71203f60c8830110326a21c9376c0f9099e
tirumalasivannarayana <tirumalasivannarayana@gmail.com>
2020-05-01 17:25:15 +0530

    first commit

---
+++ src/pages/AdminSanctions/scheme.vue
@@ -0,0 +1,89 @@
+<template>
+  <form>
+    <md-card>
+      <md-card-header data-background-color="green">
+        <h4 class="title">Scheme details</h4>
+        <p class="category">Provide Scheme details as per AS Letter</p>
+      </md-card-header>
+
+      <md-card-content>
+        <div class="md-layout"> 
+            <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Select Organisation</label>
+
+
+              <!-- v-for loop for organisation-->
+              <md-select v-model="country" name="country" id="country" placeholder="organisation">
+                <md-option value="australia">Australia</md-option>
+                <md-option value="brazil">Brazil</md-option>
+                <md-option value="japan">Japan</md-option>
+                <md-option value="united-states">United States</md-option>
+              </md-select>
+            </md-field>
+          </div>         
+
+
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Create Scheme id   (Eg:NSP,MOP)</label>
+              <md-input v-model="schemeid" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Create Respective Year of AS (in format yy-yy(18-19))</label>
+              <md-input v-model="year" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Scheme Name</label>
+              <md-input v-model="schemename" type="text">{{ }}</md-input>
+            </md-field>
+          </div>
+                 
+          <div class="md-layout-item md-size-100">
+            <md-field maxlength="5">
+              <label>About Scheme</label>
+              <md-textarea v-model="aboutme"></md-textarea>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-size-100 text-right">
+            <md-button class="md-raised md-success">Submit</md-button>
+          </div>
+        </div>
+      </md-card-content>
+    </md-card>
+  </form>
+</template>
+<script>
+export default {
+  name: "scheme",
+  props: {
+    dataBackgroundColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      usernOrganisation: null,
+      schemeid: null,
+      years: null,
+      schemename: null,
+      address: null,
+      city: null,
+      country: null,
+      code: null,
+      aboutme:
+        " "
+    };
+
+  }
+};
+</script>
+<style></style>
+
+
+
---
+++ src/pages/UserDept.vue
@@ -0,0 +1,99 @@
+<template>
+  <form>
+    <md-card>
+      <md-card-header data-background-color="green">
+        <h4 class="title">User Department details</h4>
+        <p class="category">Provide Organisation Name</p>
+      </md-card-header>
+
+      <md-card-content>
+        <div class="md-layout">
+          
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>User Department Name (Eg: Home)</label>
+              <md-input v-model="userdepartmentname" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Email Address</label>
+              <md-input v-model="deptemailadress" type="email"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Organisation Name (Eg: DGP)</label>
+              <md-input v-model="organisationname" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Email Address</label>
+              <md-input v-model="orgemailadress" type="email"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-100">
+            <md-field>
+              <label>Adress of Organisation</label>
+              <md-input v-model="address" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>City</label>
+              <md-input v-model="city" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Country</label>
+              <md-input v-model="country" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Postal Code</label>
+              <md-input v-model="code" type="number"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-size-100">
+            <md-field maxlength="5">
+              <label>About Orgn.</label>
+              <md-textarea v-model="aboutme"></md-textarea>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-size-100 text-right">
+            <md-button class="md-raised md-success">Submit</md-button>
+          </div>
+        </div>
+      </md-card-content>
+    </md-card>
+  </form>
+</template>
+<script>
+export default {
+  name: "edit-profile-form",
+  props: {
+    dataBackgroundColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      userdepartmentname: null,
+      deptemailadress: null,     
+      organisationname: null,
+      orgemailadress: null,
+      address: null,
+      city: null,
+      country: null,
+      code: null,
+      aboutme:
+        "The Home Department works on all matters pertaining to personnel of police dept., prisons dept., fire service dept.."
+    };
+  }
+};
+</script>
+<style></style>
