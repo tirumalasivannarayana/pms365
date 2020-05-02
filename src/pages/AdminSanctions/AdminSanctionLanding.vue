f81ea757aa226db68cdfb787825abd33b8a75bcb
tirumalasivannarayana <tirumalasivannarayana@gmail.com>
2020-05-02 06:03:54 +0530

    Create AS Label

--- src/pages/AdminSanctions/AdminSanctionLanding.vue
+++ src/pages/AdminSanctions/AdminSanctionLanding.vue
@@ -1,41 +1,29 @@
 <template>
-<div class="md-layout">
+  <div class="md-layout">
     <div class="md-layout-item md-size-100">
-        <nav-tabs-card>
-            <template slot="content">
-                <span class="md-nav-tabs-title">Create:</span>
-                <md-tabs md-sync-route class="md-primary" md-alignment="left">
-                    <md-tab id="tab-adminsanction" md-label="AdminSanction">
-                    </md-tab>
-                    <md-tab id="tab-project" md-label="Project">
-                    </md-tab>
-                    <md-tab id="tab-techsanction" md-label="Tech Sanction" >
-                    </md-tab>
-                    <md-tab id="tab-work" md-label="Work">
-                    </md-tab>
-                    <md-tab id="tab-technicalapproval" md-label="Technical Approval">
-                    </md-tab>
-
-                    <md-tab id="tab-posts" md-label="Work" md-icon="cloud">
-                        <nav-tabs-table></nav-tabs-table>
-                    </md-tab>
-
-                </md-tabs>
-            </template>
-        </nav-tabs-card>
+      <nav-tabs-card>
+        <template slot="content">
+          <span class="md-nav-tabs-title">Create:</span>
+          <md-tabs md-sync-route class="md-primary" md-alignment="left">
+            <md-tab id="tab-adminsanction" md-label="AdminSanction"> </md-tab>
+            <md-tab id="tab-project" md-label="Project"> </md-tab>
+            <md-tab id="tab-techsanction" md-label="Tech Sanction"> </md-tab>
+            <md-tab id="tab-work" md-label="Work"> </md-tab>
+            <md-tab id="tab-technicalapproval" md-label="Technical Approval">
+            </md-tab>
+            <md-tab id="tab-posts" md-label="Work" md-icon="cloud"> </md-tab>
+          </md-tabs>
+        </template>
+      </nav-tabs-card>
     </div>
-</div>
+  </div>
 </template>
 <script>
+import NavTabsCard from "../../components/Cards/NavTabsCard";
 
-
-    import NavTabsCard from "../../components/Cards/NavTabsCard";
-    import NavTabsTable from "../../components/Tables/NavTabsTable";
-
-    export default{
-        components: {
-            NavTabsCard,
-            NavTabsTable
-        }
-    }
+export default {
+  components: {
+    NavTabsCard
+  }
+};
 </script>
